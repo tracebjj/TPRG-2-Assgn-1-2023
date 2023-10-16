a5951153634e5c62b4a427c26f99fc59b75557cd
tracebjj <143678225+tracebjj@users.noreply.github.com>
2023-10-15 21:17:26 -0400

    Added Starter file
    The starter file without changes

---
+++ A_V_calc_starter.py
@@ -0,0 +1,36 @@
+'''
+TPRG 2131 Fall 202* Assignment 1
+Sept, 202*
+Phil J <philip.jarvis@durhamcollege>
+
+Use this template as the start
+
+*********
+A/V calculator
+
+(Level 0)
+Enter Q/q to quit – select either will gracefully close the application and cancel the calculated view option.
+Enter V/v to change the calculated view or D/d for default view.
+
+	(Level 1)
+    1.	First Area/Volume* calulation
+    2.	Second Area/Volume* calculation
+    3.	Third Area/Volume* calculation
+    4.	Fourth Area/Volume* calculation
+    5.	Fifth Area/Volume* calculation
+
+*********
+
+The above menu style (inside the asterix's) is expected, only 2 key entries to use the calculator.
+
+The menu Level 1 , options 1...5 should be modified from the above to reflect the
+function you selected.
+
+After selection at level 1, the calculator expects data to entered (use appropiate data types).
+
+(The Level 0 & 1 labels are for indication purpose and are not
+to be included)
+
+'''
+
+from assertions import add2
