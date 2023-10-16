2864e2015733dcc41813811683345193ba1df28c
tracebjj <143678225+tracebjj@users.noreply.github.com>
2023-10-15 21:35:03 -0400

    Adjustments and additions
    Adjusted Program layout, added 6th option for even 2 per option (I couldn't help it)

---
+++ A_V_calc1.2.py
@@ -0,0 +1,24 @@
+'''
+TPRG 2131 Fall 2023 Assignment 1
+October, 2023
+Trace C <trace.carter@dcmail.ca>
+'''
+import math
+from time import sleep
+
+LEVELSELECTION = input("""A/V Calculator
+ Enter Q/q to quit – select either will gracefully close the application and cancel the calculated view option.
+ Enter V/v to change the calculated view or D/d for default view.
+ """ ).lower()
+EQUATIONSELECT = input("""Select a function:
+    1.	Rectangular Prism Volume Calulation - V=whl
+    2.	Rectangular Prism Area Calculation - A=2*(wl+hl+hw)
+    3.	Circular Cone Volume Calculation - π(r^2)(h/3)
+    4.	Circular Cone Area Calculation - πr[r+sqr(h^2+r^2)]
+    5.	Sphere Volume Calculation - (4/3)πr^3
+    6.	Sphere Area Calcultation - 4πr^2""")
+while True:
+    if LEVELSELECTION == 'q':
+        sleep(0.1)
+    elif LEVELSELECTION == 'v':
+        print("")
